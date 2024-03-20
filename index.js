2cd1417670561a08a927b88e8df6346e05e03cf1
CALEB KOMEN <komencaleb502@gmail.com>
2024-03-20 12:53:38 +0300

    arrow

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const divide= (parameter1, parameter2) => {
+
+
+    return parameter1/parameter2;
+  }
+  console.log(divide(2000,100));
+
+  const square=par=> `${par}`**2;
+  
+const add = (a,b) =>{
+  return a+b
+} 
